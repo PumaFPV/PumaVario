4b35cb2a236464b480135ca8ecbd8de0f83d11ff
PumaFPV <nicolas18090@gmail.com>
2021-02-11 23:14:44 +0100

    fixed copy paste issue

--- src/vario.h
+++ src/vario.h
@@ -75,53 +75,13 @@ void vario_loop()
     deltaPa = avPa2 - avPa1;
     vario = varioKalmanFilter.updateEstimate(deltaPa);
 
-    Serial.print("Vario: ");
+    Serial.print("");
     Serial.print(deltaPa * 10);
-    Serial.print("    kalman: ");
+    Serial.print(", ");
     Serial.print(vario * 10);
-    Serial.print("    Atli: ");
-    Serial.println(ms5611.getAltitude(ms5611.readPressure(), 101325) - 532);
+    Serial.print(", ");
+    Serial.println(ms5611.getAltitude(ms5611.readPressure(), 101325) - 532 + 194);
     delay(10);
-
-/*
-    if(vario > 0)
-    {
-      expo_vario = algo_expo_mes_couilles(vario);
-    }
-    else
-    {
-      expo_vario = -algo_expo_mes_couilles(-vario);
-    }
-*/
-  tabPa2[9] = ms5611.getAltitude(ms5611.readPressure());
-  sumPa1 = 0;
-  sumPa2 = 0;
-  for (int i = 0; i <= 9; i++)
-  {
-    sumPa1 += tabPa1[i];
-    sumPa2 += tabPa2[i];
-  }
-  sumPa1 -= (find_min(tabPa1) + find_max(tabPa1));
-  sumPa2 -= (find_min(tabPa2) + find_max(tabPa2));
-  avPa1 = sumPa1/8;
-  avPa2 = sumPa2/8;
-  deltaPa = avPa2 - avPa1;
-  vario = varioKalmanFilter.updateEstimate(deltaPa);
-  Serial.print("    Vario: ");
-  Serial.print(deltaPa * 10);
-  Serial.print("    kalman: ");
-  Serial.println(vario * 10);
-  delay(10);
-
-  if(vario > 0)
-  {
-    vario_vitesse = algo_expo_mes_couilles(vario);
-  }
-  else
-  {
-    vario_vitesse = -algo_expo_mes_couilles(-vario);
-  }
-
 
   /*
   if(vario_kalman < vario_min_threshold)  // sinking
